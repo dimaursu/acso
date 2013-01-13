45804a8a0eb3e63fa7bd1bf1024a9d83bc1b16a3
Ursu Dumitru <dimaursu16@gmail.com>
2013-01-13 14:46:13 +0200

    Added line wrap,newline. making a working gets

--- kmain.c
+++ kmain.c
@@ -3,11 +3,18 @@
 #include "lib/video.c"
 
 int main( void )
-{  
+{
+  char command[30];
   putc('q');
   puts("trololoololololooloo");
-
-  puts("enter your command here $");
+  puts("aceasta e o linie mult mai lunga ca de obicei. sa vedem daca are 80 caracterei. 77. mai trebuie cateva. de fapt sunt destule. am prevazut deja line wrapping");
+  puts("aici ar trebui sa fie un newline\n");
+  puts("enter your command here $\n");
+  puts("string ads\n");
+  puts("this should be on a new line");
+  clrscr();
+  gets(command);
+  puts(command);
 
   for(;;);
 }
--- lib/video.c
+++ lib/video.c
@@ -3,14 +3,18 @@
 #define SCREEN_MAX_CHAR 1920
 #define SCREEN_MAX_MEM 3840
 
+int row = 0;
+int col = 0;
 int position = 0;
 
 void putc( char c)
 {
+    if (col == 160) {row++; col =0;}
+    position = (row * 80 * 2) + col;
     char *vidmem = (char *) 0xb8000; //a pointer to video memory
     vidmem[position] = c;
-    vidmem[position+1] = 0x7; 
-    position = position +2;
+    vidmem[position+1] = 0x7;
+    col = col + 2;
 }
 
 void puts( char *string )
@@ -18,24 +22,48 @@ void puts( char *string )
   int i=0;
    while(string[i] != '\0')
    {
-       putc(string[i]);
-       i++;
-   }
-}
-
-int clrscr()
-{
-    int i = 0;
-    char *vidmem = (char*) 0xb800; 
-    for(i=0; i<200; i++)
-    {
-       vidmem[i] = ' ';
+      //if ((string[i] == 92)&&(string[i+1]==110)) //those are ascii codes for \n
+      if (string[i]=='\n')
+        {
+          row++;
+          col=0;
+          i++;
+        }
+      else
+      {
+        putc(string[i]);
+        i++;
+      }
     }
-    return 0;
+  }
+
+void clrscr()
+{
+  char *vidmem = (char *)0xB8000;
+  int scr_size = 80*25;
+  int i;
+
+  for (i=0; i<scr_size; i++) 
+  {
+    *vidmem++ = 0;
+    *vidmem++ = 0xF;
+  }
+  row=0; col=0;
 }
 
-char* gets(char *c)
+void gets( char *str )
 {
+ int i=0;
+ while(i<=32)
+    {
+      str[i]=getchar();
+      if((i==32) || ( str[i]=='\n') ||  ( str[i]=='\0') )
+      {
+        str[i]='\0'; return;
+      }
 
-  return *c;
+      putc(str[i]); // adding echo
+
+    i++;
+    }
 }
