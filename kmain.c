5dabf8aa187ee279bfcb341561845ab6157bf687
Ursu Dumitru <dimaursu16@gmail.com>
2013-01-14 04:28:05 +0200

    Added function for cursor movement. Using gets() to acomplish the task

--- kmain.c
+++ kmain.c
@@ -4,17 +4,14 @@
 
 int main( void )
 {
-  char command[30];
-  putc('q');
-  puts("trololoololololooloo");
-  puts("aceasta e o linie mult mai lunga ca de obicei. sa vedem daca are 80 caracterei. 77. mai trebuie cateva. de fapt sunt destule. am prevazut deja line wrapping");
-  puts("aici ar trebui sa fie un newline\n");
-  puts("enter your command here $\n");
-  puts("string ads\n");
-  puts("this should be on a new line");
-  clrscr();
-  gets(command);
-  puts(command);
+  char command[200];
+    for(;;)
+    {
+      puts("Enter a command:$");
+      gets(command);
+      puts("\nYou typed:\n");
+      puts(command);
+      puts("\n");
+    }
 
-  for(;;);
 }
--- lib/video.c
+++ lib/video.c
@@ -7,6 +7,17 @@ int row = 0;
 int col = 0;
 int position = 0;
 
+ void update_cursor(int row, int col)
+ {
+    unsigned short pos=(row*80) + col;
+    // cursor LOW port to vga INDEX register
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (unsigned char)(pos&0xFF));
+    // cursor HIGH port to vga INDEX register
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (unsigned char )((pos>>8)&0xFF));
+ }
+
 void putc( char c)
 {
     if (col == 160) {row++; col =0;}
@@ -14,6 +25,7 @@ void putc( char c)
     char *vidmem = (char *) 0xb8000; //a pointer to video memory
     vidmem[position] = c;
     vidmem[position+1] = 0x7;
+    update_cursor(row, col/2+1);
     col = col + 2;
 }
 
@@ -35,7 +47,7 @@ void puts( char *string )
         i++;
       }
     }
-  }
+}
 
 void clrscr()
 {
